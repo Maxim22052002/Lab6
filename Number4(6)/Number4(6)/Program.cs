3cf68c5be76fa76dd65a846686755dde8266ea7f
Maxim22052002 <56323419+Maxim22052002@users.noreply.github.com>
2020-05-11 15:29:46 +0300

    Task4

---
+++ Number4(6)/Number4(6)/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Number4_6_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"C:\Users\USER\Desktop\Lab6\Number4(6)\Lab6_Temp");
+            FileInfo fileInfo = new FileInfo(@"C:\Users\USER\Desktop\Lab6\Number1(6)\lab.dat");
+            if (!File.Exists(@"C:\Users\USER\Desktop\Lab6\Number4(6)\Lab6_Temp\lab.dat"))
+            {
+                fileInfo.CopyTo(@"C:\Users\USER\Desktop\Lab6\Number4(6)\Lab6_Temp\lab.dat");
+            }
+            
+            else if(File.Exists(@"C:\Users\USER\Desktop\Lab6\Number4(6)\Lab6_Temp\lab.dat"))
+            {
+                File.Delete(@"C:\Users\USER\Desktop\Lab6\Number4(6)\Lab6_Temp\lab.dat");
+                fileInfo.CopyTo(@"C:\Users\USER\Desktop\Lab6\Number4(6)\Lab6_Temp\lab.dat");
+
+            }
+            
+            FileStream file = new FileStream(@"C:\Users\USER\Desktop\Lab6\Number4(6)\Lab6_Temp\lab.dat", FileMode.Open);
+            BinaryReader reader = new BinaryReader(file);
+            FileStream newfile = new FileStream(@"C:\Users\USER\Desktop\Lab6\Number4(6)\Lab6_Temp\lab_backup.dat", FileMode.Create);
+            BinaryWriter writer = new BinaryWriter(newfile);
+            for(int i = 0; i<file.Length; i++)
+            {
+                byte bmp = reader.ReadByte();
+                writer.Write(bmp);
+            }
+            
+            writer.Close();
+            reader.Close();
+            FileInfo info = new FileInfo(@"C:\Users\USER\Desktop\Lab6\Number4(6)\Lab6_Temp\lab.dat");
+            Console.WriteLine("The size: "+info.Length);
+            Console.WriteLine("Last change time: "+info.LastWriteTime);
+            Console.WriteLine("Last access time: " + info.LastAccessTime);
+        }
+    }
+}
