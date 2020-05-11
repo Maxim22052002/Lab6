c75fbfd022465f09068b7d8b5365de1b3ae72c56
Maxim22052002 <56323419+Maxim22052002@users.noreply.github.com>
2020-05-11 15:16:21 +0300

    Task3

---
+++ Number3(6)/Number3(6)/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace Number3_6_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader readfile = File.OpenText("testFile.txt");
+            int counter = 0;
+            string str = readfile.ReadToEnd();
+            char[] array = str.ToCharArray();
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == '<')
+                {
+                    array[i] = '{';
+                    counter++;
+                }
+                if (array[i] == '>')
+                {
+                    array[i] = '}';
+                    counter++;
+                }
+                
+
+            }
+            str = String.Concat(array);
+            readfile.Close();
+            StreamWriter newfile = File.CreateText("newtestFile.txt");
+            Console.WriteLine();
+            newfile.WriteLine(str);
+            newfile.Close();
+            Console.WriteLine("Number of changes: "+counter);
+            
+
+            
+            
+            
+        }
+    }
+}
